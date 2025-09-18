2f35cf32478a3eb4594aba656861d9c5042a59b1
vamseeuxd <vamsee.uxd@gmail.com>
2025-09-18 07:39:08 +0530

    chore: update Angular Fire to version 20.0.1 and configure Firebase services
    feat: add theme-color meta tags for light and dark modes in index.html
    
    feat: initialize Firebase app and provide necessary services in main.ts
    
    style: update theme variables in variables.scss for improved color scheme

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,12 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { isDevMode } from '@angular/core';
 import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -25,6 +31,22 @@ bootstrapApplication(AppComponent, {
       withPreloading(PreloadAllModules),
       withHashLocation()
     ),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyBaCbG32nzzhejYZHO-gBR6sj0OWXRrAmo',
+        authDomain: 'playgrouping.firebaseapp.com',
+        projectId: 'playgrouping',
+        storageBucket: 'playgrouping.firebasestorage.app',
+        messagingSenderId: '1051851135943',
+        appId: '1:1051851135943:web:83132dc6478f2e1d8897e2',
+        measurementId: 'G-W5FYZB7RV6',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerImmediately',
